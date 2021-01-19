5f54a05511a6e7b4cc5013491d042bace6de8daa
likunzhu <likunzhu@sinobest.cn>
2021-01-19 18:13:56 +0800

    add:新增登录页面,个人中心页面及功能

--- js/appConfig.js
+++ js/appConfig.js
@@ -3,6 +3,6 @@ window.appConfig = {
 	/**
 	 * 真实的后端地址
 	 */
-	serviceUrl: 'http://192.168.31.111:8080',
+	serviceUrl: 'http://192.168.66.47:8080',
 	
 }
--- js/utils.js
+++ js/utils.js
@@ -32,5 +32,50 @@ window.utils = {
 		plus.nativeUI.toast(msg, {verticalAlign:"center"});
 		
 	},
+	/**
+	 * 将当期登录用户添加到缓存中去,storge存储是以字符串格式来存的，需要先转换成字符串，
+	 * 如果以对象存进去，会获取不到值
+	 * @param {Object} 
+	
+	 */
+	setCurrentUser: function(currentUser) {
+		var str = JSON.stringify(currentUser)
+		plus.storage.setItem("currentUser",str);
+		// plus.storage.setItem("currentUser",currentUser);
+	},
+	/**
+	 * 获得当前登录用户
+	 * @return {Object} 
+	
+	 */
+	getCurrentUser: function() {
+		var str= plus.storage.getItem("currentUser")
+		return JSON.parse(str);
+	},
+	/**
+	 * 通过缓存重新登录
+	 * @return {Object} 
+	
+	 */
+	reLoginByStorage:function(){
+		var currentUser = utils.getCurrentUser();
+		if(currentUser!=null){
+			mui.openWindow("index.html");
+		}
+	},
+	/**
+	 * 通过缓存重新登录
+	 * @return {Object} 
+	
+	 */
+	logOut:function(){
+		mui.confirm("是否退出当前登录用户", "系统提示",['取消','确认'],function (e) {
+			if(e.index==1){//'取消'按钮下标为1.确认按钮下标为2
+				plus.storage.clear();
+				mui.openWindow("login.html");
+			}
+			
+		},'div')
+	}
 	
 }
